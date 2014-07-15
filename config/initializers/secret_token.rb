415bbecbb44f95899b4d97be5aefd9b088fcbb3d
root <root@fagailocal.net>
2014-07-16 02:06:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Falsetto::Application.config.secret_key_base = '39468a2b9daf4a9b6258739fe36cd9806fb49c5ad7fb0941141548bd56d10243b63e5ad79bd7c58fa65f9ae5580584961a03ec11069077b1fff6acfb7f6b8117'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Falsetto::Application.config.session_store :cookie_store, key: '_falsetto_session'
